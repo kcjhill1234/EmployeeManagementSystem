4c6a66ce623493c43da62c5515335f4b60c5e2c2
Kenyatta Hill <khill@insight2profit.com>
2020-07-24 23:01:34 -0400

    main menu

---
+++ src/prompts/MainMenu.js
@@ -0,0 +1,55 @@
+const inquirer = require('inquirer');
+
+const menuChoices = [
+  {
+    name: 'View All Employees',
+    value: 'viewAllEmployees'
+  },
+  {
+    name: 'View All Employees By Manager',
+    value: 'viewAllEmployeesByManager'
+  },
+  {
+    name: 'View All Employees By Department',
+    value: 'viewAllEmployeesByDepartment'
+  },
+  {
+    name: 'Add Employee',
+    value: 'addEmployee'
+  },
+  {
+    name: 'Remove Employee',
+    value: 'removeEmployee'
+  },
+  {
+    name: 'Update Employee Role',
+    value: 'updateEmployeeRole'
+  },
+  {
+    name: 'Update Employee Manager',
+    value: 'updateEmployeeManager'
+  },
+  {
+    name: 'View All Roles',
+    value: 'viewAllRoles'
+  },
+  {
+    name: 'Add Role',
+    value: 'addRole'
+  },
+  {
+    name: 'Remove Role',
+    value: 'removeRole'
+  },
+]
+const questions = [
+  {
+    type: 'list',
+    name: 'menu',
+    message: 'What would you like to do?',
+    choices: menuChoices
+  },
+]
+
+
+const MainMenu = () => inquirer.prompt(questions);
